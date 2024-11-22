92b3907562357e568b6b788dfbc8ea4aee071b79
dursuninel <drsinl1905@gmail.com>
2024-11-22 16:40:01 +0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+$("header").html(`<div class="top_header"></div>
+      <div class="top_nav">
+        <div class="container">
+          <div class="header_flex">
+            <div class="header_bars">
+              <div class="bar">Menü</div>
+            </div>
+            <div class="header_social">
+              <ul>
+                <li>
+                  <a href="/" title="instagram"
+                    ><i class="fa-brands fa-instagram"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="/" title="facebook"
+                    ><i class="fa-brands fa-facebook-f"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="/" title="linkedin"
+                    ><i class="fa-brands fa-linkedin-in"></i
+                  ></a>
+                </li>
+              </ul>
+            </div>
+            <div class="header_logo">
+              <a href="/" title="DroneSoccer"><img src="img/logo.png" alt="Logo" /></a>
+            </div>
+            <div class="header_phone">
+              <a href="/" class="header_phone" title="Hemen Ulaşın">
+                <div>
+                  <i class="fa-solid fa-phone"></i>
+                </div>
+                <div>
+                  <span class="phone_text">Hemen Ulaşın</span>
+                  <span class="phone_number">0545 504 0565</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="header_links">
+          <ul>
+            <li>
+              <a href="/" title="Anasayfa">Anasayfa</a>
+            </li>
+            <li>
+              <a href="/hakkimizda.html" title="Hakkımızda">Hakkımızda</a>
+            </li>
+            <li>
+              <a href="/haberler.html" title="Haberler">Haberler</a>
+            </li>
+            <li>
+              <a href="/topluluk.html" title="Topluluk">Topluluk</a>
+            </li>
+            <li>
+              <a href="/takimlar.html" title="Takımlar">Takımlar</a>
+            </li>
+            <li>
+              <a href="/ligler.html" title="Ligler">Ligler</a>
+            </li>
+            <li>
+              <a href="/etkinlikler.html" title="Etkinlikler">Etkinlikler</a>
+            </li>
+            <li>
+              <a href="/is-birlikleri.html" title="İş Birlikleri">İş Birlikleri</a>
+            </li>
+            <li>
+              <a href="/iletisim.html" title="İletişim">İletişim</a>
+            </li>
+          </ul>
+        </div>
+        <div class="header_overlay"></div>
+      </div>`);
 $(".bar").click(function () {
   $(".header_links, .header_overlay").addClass("open");
 });
@@ -25,8 +102,8 @@ var teams_slider = new Swiper(".teams_slider", {
   slidesPerView: 3,
   spaceBetween: 16,
   navigation: {
-    nextEl: '.teams_arrow_right',
-    prevEl: '.teams_arrow_left',
+    nextEl: ".teams_arrow_right",
+    prevEl: ".teams_arrow_left",
   },
   breakpoints: {
     0: {
@@ -40,4 +117,3 @@ var teams_slider = new Swiper(".teams_slider", {
     },
   },
 });
-;
